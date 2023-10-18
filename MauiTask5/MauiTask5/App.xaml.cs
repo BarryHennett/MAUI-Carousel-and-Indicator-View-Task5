8beda074a2cb421e6e765d4df3da4a58f1b09129
BarryHennett <124011454+BarryHennett@users.noreply.github.com>
2023-10-18 14:01:27 +1300

    Complete

---
+++ MauiTask5/MauiTask5/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiTask5;
+    using MauiTask5.Pages;
+
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new CarouselViewDemo();
+        }
+    }
